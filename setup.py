db5e9d46a8bb3bac5742f5661cec38ed0211da5e
Uchida Ryo <uchidaryo.gm@gmail.com>
2022-10-27 08:19:35 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'my_ros2_examples_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Uchida Ryo',
+    maintainer_email='uchidaryo.gm@gmail.com',
+    description='my ROS2 Python examples',
+    license='BSD-3-Clause',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
